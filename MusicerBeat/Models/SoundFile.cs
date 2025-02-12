d1c728d78603dced487d3e637971ff697c24d010
Akn <sibata486@gmail.com>
2025-02-12 14:54:02 +0900

    add class / サウンドファイルを表すクラスを追加。

---
+++ MusicerBeat/Models/SoundFile.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Linq;
+using Prism.Mvvm;
+
+namespace MusicerBeat.Models
+{
+    public class SoundFile : BindableBase
+    {
+        private string fullName;
+        private string name;
+        private int duration;
+        private int listenCount;
+        private bool isSkip;
+        private bool playing;
+
+        public SoundFile(string filePath)
+        {
+            var extension = Path.GetExtension(filePath).ToLower();
+            if (!new[] { ".wav", ".mp3", ".ogg", }.Contains(extension))
+            {
+                throw new ArgumentException("入力されたファイルがサウンドファイルではありません");
+            }
+
+            FullName = filePath;
+            Name = Path.GetFileName(filePath);
+            Extension = extension;
+        }
+
+        public string FullName { get => fullName; set => SetProperty(ref fullName, value); }
+
+        public string Name { get => name; set => SetProperty(ref name, value); }
+
+        public string Extension { get; set; }
+
+        public int Duration { get => duration; set => SetProperty(ref duration, value); }
+
+        public int ListenCount { get => listenCount; set => SetProperty(ref listenCount, value); }
+
+        public bool IsSkip { get => isSkip; set => SetProperty(ref isSkip, value); }
+
+        public bool Playing { get => playing; set => SetProperty(ref playing, value); }
+    }
+}
