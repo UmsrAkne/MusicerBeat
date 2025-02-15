a7357177232477c6d4a307aabc9521a575bafb99
Akn <sibata486@gmail.com>
2025-02-15 18:55:36 +0900

    Update version number.

--- MusicerBeat/Models/TextWrapper.cs
+++ MusicerBeat/Models/TextWrapper.cs
@@ -30,9 +30,9 @@ namespace MusicerBeat.Models
         private void SetVersion()
         {
             const int major = 0;
-            const int minor = 3;
+            const int minor = 4;
             const int patch = 0;
-            const string date = "20250214";
+            const string date = "20250215";
             const string suffixId = "a";
 
             Version = $"{major}.{minor}.{patch} ({date}{suffixId})";
