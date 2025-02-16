95e618a7ec7b8c74def176188e788204f2d6f701
Akn <sibata486@gmail.com>
2025-02-16 15:13:54 +0900

    add feat / VolumeController.ChangeVolumes を実装。

--- MusicerBeat/Models/VolumeController.cs
+++ MusicerBeat/Models/VolumeController.cs
@@ -1,35 +1,106 @@
+using System;
 using System.Collections.Generic;
 
 namespace MusicerBeat.Models
 {
     public class VolumeController
     {
+        private float volumeFadeStep = 0.1f;
+
+        public float CurrentVolume { get; set; } = 1.0f;
+
+        /// <summary>
+        /// 一度の処理で変化させるボリュームの量です。セットされる値は、内部で絶対値に変換されます。<br/>
+        /// デフォルトは 0.1 です。
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">ゼロをセットしたときにすろーされます。</exception>
+        public float VolumeFadeStep
+        {
+            get => volumeFadeStep;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "VolumeFadeStep cannot be 0!");
+                }
+
+                value = Math.Abs(value);
+                volumeFadeStep = value;
+            }
+        }
+
         private List<ISoundPlayer> SoundPlayers { get; set; } = new ();
 
         /// <summary>
         /// このクラスがボリュームを調節する対象を追加します。<br/>
-        /// ただし、内部で保持できるオブジェクトは2つまでです。それ以上の数になる場合は内部のリストをクリアしてから追加します。
+        /// ただし、内部で保持できるオブジェクトは2つまでです。それより多い数になる場合は古いアイテムを削除して追加します。
         /// </summary>
         /// <param name="player">追加するプレイヤー。</param>
         public void Add(ISoundPlayer player)
         {
             if (SoundPlayers.Count >= 2)
             {
-                SoundPlayers.Clear();
+                SoundPlayers.RemoveAt(0);
+                SoundPlayers.Add(player);
             }
 
             SoundPlayers.Add(player);
         }
 
         /// <summary>
-        /// 一定のロジックに従って、このクラスが保持している SoundPlayer のボリュームを調節します。
+        /// 現在のサウンドプレイヤーの音量をフェードさせ、
+        /// フェードアウトが完了した古いプレイヤーをリストから削除します。
         /// </summary>
+        /// <remarks>
+        ///  - `SoundPlayers` に 2 つのプレイヤーが存在する場合にのみ動作します。<br/>
+        ///  - 古いプレイヤーの音量を `VolumeFadeStep` ずつ減少させ、 0 になったらリストから削除します。<br/>
+        ///  - 新しいプレイヤーの音量を `VolumeFadeStep` ずつ増加させ、`CurrentVolume` まで上げます。<br/>
+        ///  - すでにフェードが完了している場合は処理をスキップします。<br/>
+        /// </remarks>
         public void ChangeVolumes()
         {
-            if (SoundPlayers.Count <= 1)
+            if (SoundPlayers.Count != 2)
             {
                 return;
             }
+
+            var oldPlayer = SoundPlayers[0];
+            var newPlayer = SoundPlayers[1];
+
+            if (oldPlayer.Volume == 0 && Math.Abs(newPlayer.Volume - CurrentVolume) < 0.001)
+            {
+                return;
+            }
+
+            if (oldPlayer.Volume > 0)
+            {
+                oldPlayer.Volume -= VolumeFadeStep;
+                oldPlayer.Volume = Math.Max(0, oldPlayer.Volume);
+            }
+
+            if (newPlayer.Volume < CurrentVolume)
+            {
+                newPlayer.Volume += VolumeFadeStep;
+                newPlayer.Volume = Math.Min(CurrentVolume, newPlayer.Volume);
+            }
+
+            if (IsVolumeFadeCompleted())
+            {
+                SoundPlayers.RemoveAt(0);
+            }
+        }
+
+        private bool IsVolumeFadeCompleted()
+        {
+            if (SoundPlayers.Count != 2)
+            {
+                return false;
+            }
+
+            var oldPlayer = SoundPlayers[0];
+            var newPlayer = SoundPlayers[1];
+
+            return oldPlayer.Volume == 0 && Math.Abs(newPlayer.Volume - CurrentVolume) < 0.001;
         }
     }
 }
---
+++ MusicerBeatTests/Models/VolumeControllerTest.cs
@@ -0,0 +1,86 @@
+using MusicerBeat.Models;
+using MusicerBeatTests.ViewModels;
+
+namespace MusicerBeatTests.Models
+{
+    [TestFixture]
+    public class VolumeControllerTest
+    {
+        [Test]
+        public void ChangeVolumeTest()
+        {
+            var p1 = new MockSoundPlayer(){ Volume = 1, };
+            var p2 = new MockSoundPlayer(){ Volume = 0, };
+            var vc = new VolumeController() { VolumeFadeStep = 0.4f, };
+            vc.Add(p1);
+            vc.Add(p2);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(p1.Volume, Is.EqualTo(1));
+                Assert.That(p2.Volume, Is.EqualTo(0));
+            });
+
+            vc.ChangeVolumes();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(p1.Volume, Is.EqualTo(0.6f).Within(0.0001f));
+                Assert.That(p2.Volume, Is.EqualTo(0.4f).Within(0.0001f));
+            });
+
+            vc.ChangeVolumes();
+            vc.ChangeVolumes();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(p1.Volume, Is.EqualTo(0));
+                Assert.That(p2.Volume, Is.EqualTo(1f));
+            });
+        }
+
+        [Test]
+        public void ChangeVolumeTest_3Add()
+        {
+            var p1 = new MockSoundPlayer(){ Volume = 1, };
+            var p2 = new MockSoundPlayer(){ Volume = 0, };
+            var p3 = new MockSoundPlayer(){ Volume = 0, };
+
+            var vc = new VolumeController() { VolumeFadeStep = 0.4f, };
+            vc.Add(p1);
+            vc.Add(p2);
+
+            vc.ChangeVolumes();
+            vc.ChangeVolumes();
+            vc.ChangeVolumes();
+
+            // 新しいオブジェクトを投入。
+            vc.Add(p3);
+
+            // 一度目の音量調節処理が完了。それぞれの音量は以下の状態。
+            Assert.Multiple(() =>
+            {
+                Assert.That(p1.Volume, Is.EqualTo(0)); // 登録解除済み。
+                Assert.That(p2.Volume, Is.EqualTo(1)); // 現在再生中。
+                Assert.That(p3.Volume, Is.EqualTo(0)); // 再生待ち。
+            });
+
+            vc.ChangeVolumes();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(p2.Volume, Is.EqualTo(0.6f).Within(0.0001f)); // 音量ダウン
+                Assert.That(p3.Volume, Is.EqualTo(0.4f).Within(0.0001f)); // 音量アップ
+            });
+
+            vc.ChangeVolumes();
+            vc.ChangeVolumes();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(p2.Volume, Is.EqualTo(0));
+                Assert.That(p3.Volume, Is.EqualTo(1f));
+            });
+        }
+    }
+}
