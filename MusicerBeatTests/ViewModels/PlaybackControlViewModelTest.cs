0b7e37e8bf81f0dad6e446b8c1c26873a5fcf0bb
Akn <sibata486@gmail.com>
2025-02-24 00:37:30 +0900

    add test / ボリューム変遷のテストを追加した。

--- MusicerBeatTests/ViewModels/PlaybackControlViewModelTest.cs
+++ MusicerBeatTests/ViewModels/PlaybackControlViewModelTest.cs
@@ -161,5 +161,93 @@ namespace MusicerBeatTests.ViewModels
             // 全ての再生が終了したはずなので、プレイヤーが停止状態かを確認する。
             Assert.That(vm.GetStatus(), Is.EqualTo(PlayingStatus.Stopped));
         }
+
+        [Test]
+        public void PlayCommand_VolumeTransitionTest_allLongSound()
+        {
+            var testData = new List<(string soundFilePath, int duration, string playerName)>
+            {
+                (@"C:\test\a.mp3", 2, "p1"),
+                (@"C:\test\b.mp3", 2, "p2"),
+                (@"C:\test\c.mp3", 2, "p3"),
+                (@"C:\test\d.mp3", 2, "p4"),
+            };
+
+            var playList = new MockPlaylist();
+            foreach (var data in testData)
+            {
+                playList.OriginalList.Add(new SoundFile(data.soundFilePath) { Duration = data.duration, });
+            }
+
+            var ps =
+                testData.Select(d => new MockSoundPlayer() { Name = d.playerName, }).ToList();
+
+            var soundPlayerFactory = new DummySoundPlayerFactory { PlayerSource = ps, };
+
+            var vm = new PlaybackControlViewmodel(playList, soundPlayerFactory);
+            vm.CrossFadeDuration = TimeSpan.FromSeconds(1);
+            vm.VolumeController.VolumeFadeStep = 0.25f;
+
+            // プレイヤーが止まっているかチェック。
+            Assert.That(vm.GetStatus(), Is.EqualTo(PlayingStatus.Stopped));
+
+            vm.PlayCommand.Execute(null);
+
+            var expectedTransitions = new List<(TimeSpan, double?, double?, string)>
+            {
+                (TimeSpan.FromMilliseconds(1000), 1.0, null, "p1"),
+
+                (TimeSpan.FromMilliseconds(250), 0.75, 0.25, "p1, p2 (1) クロスフェード開始"),
+                (TimeSpan.FromMilliseconds(250), 0.5,  0.5 , "p1, p2 (2)"),
+                (TimeSpan.FromMilliseconds(250), 0.25, 0.75, "p1, p2 (3)"),
+                (TimeSpan.FromMilliseconds(250), 0,    1.0 , "p1, p2 (4) クロスフェード完了"),
+
+                (TimeSpan.FromMilliseconds(250), 0.75, 0.25, "p2 p3 (1) クロスフェード開始"),
+                (TimeSpan.FromMilliseconds(250), 0.5,  0.5,  "p2 p3 (2)"),
+                (TimeSpan.FromMilliseconds(250), 0.25, 0.75, "p2 p3 (3)"),
+                (TimeSpan.FromMilliseconds(250), 0,    1.0,  "p2 p3 クロスフェード終了"),
+
+                (TimeSpan.FromMilliseconds(250), 0.75, 0.25, "p3 p4 (1) クロスフェード開始"),
+                (TimeSpan.FromMilliseconds(250), 0.5,  0.5,  "p3 p4 (2)"),
+                (TimeSpan.FromMilliseconds(250), 0.25, 0.75, "p3 p4 (3)"),
+                (TimeSpan.FromMilliseconds(250), 0,    1.0,  "p3 p4 クロスフェード終了"),
+
+                (TimeSpan.FromMilliseconds(1000), 1.0, null, "p4 end,"),
+            };
+
+            foreach (var (time, expectedVolOld, expectedVolNew,description) in expectedTransitions)
+            {
+                vm.UpdatePlaybackState();
+
+                var ov = vm.GetVolumes().OldPlayerVol;
+                var nv = vm.GetVolumes().NewPlayerVol;
+
+                Assert.Multiple(() =>
+                {
+                    if (expectedVolOld is null)
+                    {
+                        Assert.That(ov, Is.Null);
+                    }
+                    else
+                    {
+                        Assert.That(ov, Is.EqualTo(expectedVolOld).Within(0.0001));
+                    }
+
+                    if (expectedVolNew is null)
+                    {
+                        Assert.That(nv, Is.Null);
+                    }
+                    else
+                    {
+                        Assert.That(nv, Is.EqualTo(expectedVolNew).Within(0.0001));
+                    }
+                });
+
+                soundPlayerFactory.PlayerSource.ForEach(p => p.CurrentTime += time);
+            }
+
+            // 全ての再生が終了したはずなので、プレイヤーが停止状態かを確認する。
+            Assert.That(vm.GetStatus(), Is.EqualTo(PlayingStatus.Stopped));
+        }
     }
 }
