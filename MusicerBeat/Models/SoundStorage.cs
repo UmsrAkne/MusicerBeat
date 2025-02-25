b7d2e45af80f7efbc4ca27fa252ec6499ce5ec25
Akn <sibata486@gmail.com>
2025-02-25 21:55:01 +0900

    add feat / m3u ファイルからサウンドのリストをロードして表示する処理を実装した。

--- MusicerBeat/Models/SoundStorage.cs
+++ MusicerBeat/Models/SoundStorage.cs
@@ -18,6 +18,7 @@ namespace MusicerBeat.Models
             {
                 if (SetProperty(ref fullPath, value))
                 {
+                    IsM3U = Path.GetExtension(fullPath)?.ToLower() == ".m3u";
                     RaisePropertyChanged(nameof(Name));
                 }
             }
@@ -29,13 +30,32 @@ namespace MusicerBeat.Models
             private set => SetProperty(ref name, value);
         }
 
+        public bool IsM3U { get; set; }
+
         public IEnumerable<SoundStorage> GetChildren()
         {
-            return Directory.GetDirectories(FullPath).Select(d => new SoundStorage() { FullPath = d, });
+            var list = new List<SoundStorage>();
+            if (!Directory.Exists(FullPath))
+            {
+                return list;
+            }
+
+            list.AddRange(Directory.GetDirectories(FullPath).Select(d => new SoundStorage() { FullPath = d, }));
+            list.AddRange(
+                Directory.GetFiles(FullPath)
+                    .Where(s => Path.GetExtension(s).ToLower() == ".m3u")
+                    .Select(s => new SoundStorage() { FullPath = s, }));
+
+            return list;
         }
 
         public IEnumerable<SoundFile> GetFiles()
         {
+            if (IsM3U)
+            {
+                return ParseM3U(File.ReadAllText(FullPath));
+            }
+
             return Directory.GetFiles(FullPath)
                 .Where(SoundFile.IsSoundFile)
                 .Select(d =>
