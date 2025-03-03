d63a0582facc7336c82ed99acf15cb263901fce2
Akn <sibata486@gmail.com>
2025-03-04 00:41:55 +0900

    refactoring / 再生中のファイル名の表示機能を新しいクラスに分離した。

---
+++ MusicerBeat/Models/PlayingFileNameViewer.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using MusicerBeat.Models.Interfaces;
+using Prism.Mvvm;
+
+namespace MusicerBeat.Models
+{
+    public class PlayingFileNameViewer : BindableBase
+    {
+        private string playingFileName = string.Empty;
+
+        public string PlayingFileName
+        {
+            get => playingFileName;
+            private set => SetProperty(ref playingFileName, value);
+        }
+
+        public void UpdatePlayingFileName(IEnumerable<ISoundPlayer> players)
+        {
+            var list = players.ToList();
+            if (list.Count == 0)
+            {
+                PlayingFileName = string.Empty;
+                return;
+            }
+
+            if (list.Count == 1)
+            {
+                PlayingFileName = list.First().PlayingSound?.NameWithoutExtension;
+                PlayingFileName ??= string.Empty;
+                return;
+            }
+
+            var before = list.First().PlayingSound?.NameWithoutExtension;
+            before ??= string.Empty;
+
+            var after = list.Last().PlayingSound?.NameWithoutExtension;
+            after ??= string.Empty;
+
+            PlayingFileName = $"{before} --> {after}";
+        }
+    }
+}
