e57abb21a5b3275cda0c84290089a7e6c9480c71
Akn <sibata486@gmail.com>
2025-03-04 16:18:46 +0900

    add test / PlaybackTimeString のテストを追加。

--- MusicerBeatTests/Models/PlaybackInformationViewerTest.cs
+++ MusicerBeatTests/Models/PlaybackInformationViewerTest.cs
@@ -39,5 +39,47 @@ namespace MusicerBeatTests.Models
             viewer.UpdatePlaybackInformation(players);
             Assert.That(viewer.PlayingFileName, Is.EqualTo(expectedResult));
         }
+
+        private static IEnumerable<TestCaseData> TestCases()
+        {
+            yield return new TestCaseData(
+                new List<ISoundPlayer>(),
+                string.Empty
+            ).SetName("停止状態");
+
+            yield return new TestCaseData(
+                new List<ISoundPlayer>
+                {
+                    new MockSoundPlayer(TimeSpan.FromSeconds(1))
+                    {
+                        PlayingSound = new SoundFile("a.mp3"),
+                    },
+                },
+                "00:00:01"
+            ).SetName("単体再生");
+
+            yield return new TestCaseData(
+                new List<ISoundPlayer>
+                {
+                    new MockSoundPlayer(TimeSpan.FromSeconds(4))
+                    {
+                        PlayingSound = new SoundFile("a.mp3"),
+                    },
+                    new MockSoundPlayer(TimeSpan.FromSeconds(2))
+                    {
+                        PlayingSound = new SoundFile("b.mp3"),
+                    },
+                },
+                "00:00:04 --> 00:00:02"
+            ).SetName("２曲再生");
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void PlaybackTimeStringTest(IEnumerable<ISoundPlayer> players,string expectedText)
+        {
+            var viewer = new PlaybackInformationViewer();
+            viewer.UpdatePlaybackInformation(players);
+            Assert.That(viewer.PlaybackTimeString, Is.EqualTo(expectedText));
+        }
     }
 }
