c68e1ec88c69e502d1bf920e7d1e4166e4b54e73
Akn <sibata486@gmail.com>
2025-03-04 16:02:33 +0900

    add feat / 画面下部にファイルの現在の再生位置を表示。

--- MusicerBeat/Models/PlaybackInformationViewer.cs
+++ MusicerBeat/Models/PlaybackInformationViewer.cs
@@ -7,12 +7,20 @@ namespace MusicerBeat.Models
 {
     public class PlaybackInformationViewer : BindableBase
     {
+        private readonly string shortTimeFormat = @"hh\:mm\:ss";
         private string playingFileName = string.Empty;
+        private string playbackTimeString = string.Empty;
 
         public string PlayingFileName
         {
             get => playingFileName;
             private set => SetProperty(ref playingFileName, value);
+        }
+
+        public string PlaybackTimeString
+        {
+            get => playbackTimeString;
+            set => SetProperty(ref playbackTimeString, value);
         }
 
         public void UpdatePlaybackInformation(IEnumerable<ISoundPlayer> players)
@@ -26,18 +34,22 @@ namespace MusicerBeat.Models
 
             if (list.Count == 1)
             {
+                PlaybackTimeString = list.First().CurrentTime.ToString(shortTimeFormat);
                 PlayingFileName = list.First().PlayingSound?.NameWithoutExtension;
                 PlayingFileName ??= string.Empty;
                 return;
             }
 
+            var beforeTime = list.First().CurrentTime.ToString(shortTimeFormat);
             var before = list.First().PlayingSound?.NameWithoutExtension;
             before ??= string.Empty;
 
+            var afterTime = list.Last().CurrentTime.ToString(shortTimeFormat);
             var after = list.Last().PlayingSound?.NameWithoutExtension;
             after ??= string.Empty;
 
             PlayingFileName = $"{before} --> {after}";
+            PlaybackTimeString = $"{beforeTime} --> {afterTime}";
         }
     }
 }
