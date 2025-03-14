dbac1fb09d1dc874e996e49872fdfe8b301a2904
Akn <sibata486@gmail.com>
2025-03-15 00:26:54 +0900

    add / 最低限のUIを設置した SettingPage と、その使用に必要なコードを追加。

---
+++ MusicerBeat/ViewModels/SettingPageViewModel.cs
@@ -0,0 +1,30 @@
+using System;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+
+namespace MusicerBeat.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class SettingPageViewModel : BindableBase, IDialogAware
+    {
+        public event Action<IDialogResult> RequestClose;
+
+        public string Title => "Setting Page";
+
+        public DelegateCommand CloseCommand => new (() =>
+        {
+            RequestClose?.Invoke(new DialogResult());
+        });
+
+        public bool CanCloseDialog() => true;
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
